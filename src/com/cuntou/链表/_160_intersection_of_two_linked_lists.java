7f38286755425e9ddf9ca353854ad2dd4bc5461f
Eiddiecuntou <2531773004@qq.com>
2022-07-12 08:57:23 +0800

    链表

--- src/com/cuntou/链表/_160_intersection_of_two_linked_lists.java
+++ src/com/cuntou/链表/_160_intersection_of_two_linked_lists.java
@@ -1,5 +1,8 @@
 package com.cuntou.链表;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @ClassName : _160_intersection_of_two_linked_lists  //类名
  * @Description :   //描述
@@ -18,5 +21,30 @@ public class _160_intersection_of_two_linked_lists {
 
         进阶：你能否设计一个时间复杂度 O(n) 、仅用 O(1) 内存的解决方案？
      */
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
 
+        while (headB != null) {
+            if (set.contains(headB)) return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+    //使用的双指针的方法，只要a和b相等的时候就需要退出循环，两个指针把两个都走一遍
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null ) return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
 }
